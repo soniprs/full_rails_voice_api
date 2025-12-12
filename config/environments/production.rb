0d7e6a741b7cc2b8b11d37dffd854ba96d14f95f
Praveen <pravinsoni772@gmail.com>
2025-12-12 17:33:46 +0530

    Updated config file

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   config.active_record.attributes_for_inspect = [ :id ]
 
   # Enable DNS rebinding protection and other `Host` header attacks.
-  config.hosts << "rails-production-6c48.up.railway.app"
+  Rails.application.config.hosts << "rails-production-6c48.up.railway.app"
 
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
